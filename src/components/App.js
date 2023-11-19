a0bb5a1b1b7058ff6cb3befb8f2a65533551cf28
ashim1588 <ashimmehar158@gmail.com>
2023-11-19 22:24:49 +0530

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Button, FlatList, Linking, View} from 'react-native';
+import {Button, FlatList, Linking, StyleSheet, View} from 'react-native';
 import courses from '../data/courses.json';
-import {Text} from 'react-native-paper';
+import {Appbar, Card, Paragraph, Title} from 'react-native-paper';
 
 const handleClick = link => {
   Linking.canOpenURL(link).then(supported => {
@@ -15,23 +15,44 @@ const handleClick = link => {
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Appbar style={styles.bar}>
+        <Appbar.Content title="Courses" />
+      </Appbar>
       <FlatList
         data={courses}
         renderItem={({item}) => (
-          <View>
-            <Button
-              onPress={() => {
-                handleClick(item.link);
-              }}
-              title="Tap to view course"
-            />
-            <Text>{item.title}</Text>
-          </View>
+          <Card style={styles.card}>
+            <Card.Cover source={{uri: item.image}} />
+            <Card.Content>
+              <Title>{item.title}</Title>
+              <Paragraph>{item.description}</Paragraph>
+            </Card.Content>
+            <Card.Actions>
+              <Button
+                onPress={() => {
+                  handleClick(item.link);
+                }}
+                title="Tap to view course"
+              />
+            </Card.Actions>
+          </Card>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 110,
+  },
+  bar: {
+    marginTop: 40,
+  },
+  card: {
+    marginBottom: 20,
+  },
+});
 
 export default App;
---
+++ src/styles/App.js
@@ -0,0 +1,9 @@
+const {StyleSheet} = require('react-native');
+
+const AppStyles = StyleSheet.create({
+  component: {
+    color: 'White',
+  },
+});
+
+export default AppStyles;
